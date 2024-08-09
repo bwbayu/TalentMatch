c9fb8d1886452ff694ed7c47d542b730ddd51ada
bwbayu <bwbayuuu@gmail.com>
2024-08-09 21:56:57 +0700

    feature/bayu/add config for website color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mintGreen: '#3BBA9C',
+        darkBlueGray: '#2E3047',
+        slateGray: '#43455C',
+        navyGray: '#3C3F58',
+        coolGray: '#707793',
+      },
+    },
   },
   plugins: [],
 }
