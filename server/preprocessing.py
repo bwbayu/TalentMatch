9564733e21ff6a21aa681a4e71f3d6e28d6fb7f7
bwbayu <bwbayuuu@gmail.com>
2024-08-28 13:04:26 +0700

    update preprocessing text

--- server/preprocessing.py
+++ server/preprocessing.py
@@ -4,6 +4,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from num2words import num2words
+from deep_translator import GoogleTranslator
 
 nltk.download('stopwords')
 nltk.download('wordnet')
@@ -11,7 +12,34 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('punkt')
 nltk.download('punkt_tab')
 
+translator = GoogleTranslator(source='id', target='en')
+
+def split_text(text, max_length):
+    # Fungsi untuk membagi teks menjadi bagian yang lebih kecil
+    words = text.split()
+    parts = []
+    current_part = []
+
+    for word in words:
+        if len(' '.join(current_part + [word])) <= max_length:
+            current_part.append(word)
+        else:
+            parts.append(' '.join(current_part))
+            current_part = [word]
+
+    if current_part:
+        parts.append(' '.join(current_part))
+
+    return parts
+
+def translate_batch(text, max_length=500):
+    # Fungsi untuk menerjemahkan teks dalam batch
+    parts = split_text(text, max_length)
+    translated_parts = [translator.translate(part) for part in parts]
+    return ' '.join(translated_parts)
+    
 def preprocessing_data(text):
+  text = translate_batch(text)
   text = text.lower()
 
   text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
